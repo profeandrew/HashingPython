192b3b1b2adb306e247063294905f04afe13f810
Andres Lizondro <andreslizondro@outlook.com>
2024-05-03 17:19:13 -0500

    Version 0.5

---
+++ Hashes.py
@@ -0,0 +1,28 @@
+#Exercises: Calculate Hashes
+
+
+# Write a program to calculate hashes of given text
+#message: SHA-224, SHA-256, SHA3-224, SHA3-384, Keccak-384 and
+#Whirlpool. Write your code in programming language of choice
+
+
+# Importar librerías haslib y binascii
+import hashlib, binascii
+
+hacked_hashes = {
+                    "SHA-224":"ea09ae9cc6768c50fcee903ed054556e5bfc8347907f12598aa24193",
+                    "SHA3-224":"b87f88c72702fff1748e58b87e9141a42c0dbedc29a78cb0d4a5cd81",
+                    "SHA3-384":"720aea11019ef06440fbf05d87aa24680a2153df3907b23631e7177ce620"
+                }
+                
+
+inputUser = input();
+usefullInput = text.encode("utf8")
+
+print(inputUser)
+print(usefullInput)
+
+
+
+
+
