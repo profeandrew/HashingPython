383d56ae8347b05c497098b9672b721a1b6e90a3
Andres Lizondro <andreslizondro@outlook.com>
2024-05-03 17:30:34 -0500

    testing github

--- Hashes.py
+++ Hashes.py
@@ -10,19 +10,26 @@
 import hashlib, binascii
 
 hacked_hashes = {
-                    "SHA-224":"ea09ae9cc6768c50fcee903ed054556e5bfc8347907f12598aa24193",
-                    "SHA3-224":"b87f88c72702fff1748e58b87e9141a42c0dbedc29a78cb0d4a5cd81",
-                    "SHA3-384":"720aea11019ef06440fbf05d87aa24680a2153df3907b23631e7177ce620"
-                }
-                
+    "SHA3-224":"b87f88c72702fff1748e58b87e9141a42c0dbedc29a78cb0d4a5cd81",
+    "SHA3-384":"720aea11019ef06440fbf05d87aa24680a2153df3907b23631e7177ce620"
+    }
 
-inputUser = input();
-usefullInput = text.encode("utf8")
+# usuario ingresa una palabra o frase 
+hashInputByUser = input()
 
-print(inputUser)
-print(usefullInput)
+# encode UTF-8
+usefullHash = hashInputByUser.encode("utf8")
 
+#Llamada a método sha224 que genera un hash digest tipo sha224
+sha224Hash = hashlib.sha224(usefullHash).digest()
 
+#Permite que el hashing generado sea legible 
+sha224Hash = binascii.hexlify(sha224Hash)
 
+hashSinComillas = sha224Hash.replace("'", "")
 
+print("Generated sha224 Hash:", hashSinComillas)
 
+print("Stored 224 Hash:", hacked_hashes["SHA-224"])
+
+print("Hashes match?:", hacked_hashes["SHA-224"] == hashSinComillas)
